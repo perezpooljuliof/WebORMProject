4f7796f405b5ecaedcbce4fd45a71899808b51d4
perezpooljuliof <perezpooljuliof@gmail.com>
2016-07-27 21:55:19 -0500

    Se continua con la configuracion.

--- src/main/java/mx/com/tot/mx/com/tot/config/ServletConfiguration.java
+++ src/main/java/mx/com/tot/mx/com/tot/config/ServletConfiguration.java
@@ -1,7 +1,32 @@
 package mx.com.tot.mx.com.tot.config;
 
+import com.vaadin.server.*;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+
 /**
  * Created by elver on 26/07/16.
  */
-public class ServletConfiguration {
+
+@WebServlet(value = "/*", asyncSupported = true)
+public class ServletConfiguration extends VaadinServlet implements SessionInitListener, SessionDestroyListener {
+
+    @Override
+    public void servletInitialized() throws ServletException {
+        super.servletInitialized();
+        System.out.println("servletInitialized()>>>>>");
+
+
+        getService().addSessionInitListener(this);
+        getService().addSessionDestroyListener(this);
+    }
+
+    public void sessionDestroy(SessionDestroyEvent sessionDestroyEvent) {
+        System.out.println("sessionDestroy()>>>>>");
+    }
+
+    public void sessionInit(SessionInitEvent sessionInitEvent) throws ServiceException {
+        System.out.println("sessionInit()>>>>>");
+    }
 }
